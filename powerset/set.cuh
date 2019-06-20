f9fb01d347996abf6679e7a16edf32c352c5c758
Nate Gessesse <nygesse@gfx2.cas.mnbm01.gd-ais.com>
2019-06-20 12:47:25 -0500

    Working on powerset

--- powerset/set.cuh
+++ powerset/set.cuh
@@ -17,12 +17,12 @@ class Set{
             thrust::copy(i_set.begin(),i_set.end(),h_set.begin());
             //Copy Set to Device
             thrust::copy(h_set.begin(),h_set.end(),d_set.begin());
-            vecSize_fact = fact(vecSize);
+            //vecSize_fact = fact(vecSize);
         }
 
         void display(){
             std::cout << "Size: " << vecSize << std::endl;
-            std::cout << "vecSize_fact: " << vecSize_fact << std::endl;
+            //std::cout << "vecSize_fact: " << vecSize_fact << std::endl;
             displayOriginalSet();
         }
 
@@ -71,18 +71,72 @@ class Set{
             }
         }
 
+        __device__
+        int * getLowers(int index){
+            int * lowerNeighbors;
+            cudaMalloc((void **) &lowerNeighbors, sizeof(int) * index);
+            for(int i = 0; i < index){
+                lowerNeighbors[i] = index;
+            }
+            return lowerNeighbors;
+
+        }
+
+        void initialize_cofaces(int dim){
+            //Maximum number of dimensions for a given subset
+            h_cofaces.resize(dim);
+            for(int i = 0; i < dim; i++){
+                //Each dimension has n ncr i number of sets
+                h_cofaces[i].resize(ncr(i));
+                //Each set is of length i+1
+                //ie) dim 0 has only 1 element, dim 1 has 2 elements
+                for(int j = 0; j < h_cofaces.size(); j++){
+                    h_cofaces[i][j].resize(i+1);
+                }
+            }
+            //Set d_cofaces to be the same as h_cofaces
+            d_cofaces = h_cofaces;
+        }
+
+        __device__
+        void d_setCoface(dim3 index, int pos){
+            //dimension x, coface y, position z
+            d_bivector[index.x][index.y][index.z]= pos;
+        }
+
+
+        __host__ __device__
+        int ncr(int n, int r){
+            int diff = n - r;           
+            //Eliminate the larger one to save computation time
+            if(diff > r){
+                return factRange(n,diff)/fact(r);
+            //Research for properties
+            }else if(diff == r){
+                return factRange(n,diff)/fact(r);
+            }else{
+                return factRange(n,r)/fact(diff);
+            }      
+        }
+
     private:
+        //Data Type
+        typedef thrust::host_vector<thrust::host_vector<T> > h_bivector;
+        typedef thrust::device_vector<thrust::device_vector<T> > d_bivector;
+
+
         //Data
         size_t vecSize;
         int vecSize_fact;
+        int dim;
+
         thrust::host_vector<T> h_set;
         thrust::device_vector<T> d_set;
-
-        thrust::host_vector<T> h_powerset; 
-        thrust::device_vector<T> d_powerset;
-
+        thrust::host_vector<h_bivector> h_cofaces;
+        thrust::device_vector<d_bivector> d_cofaces;
 
         //Private Functions
+        __host__ __device__
         int fact(int f){
             int fac = 1;
 
@@ -92,9 +146,45 @@ class Set{
 
             return fac;
         }
-       
-       
-        int ncr(int f){
-            return vecSize_fact/(fact(f) * fact(vecSize - f));
+
+        __host__ __device__
+        int factRange(int top, int bottom){
+            int fac = 1;
+            for(int i = bottom; i <= top; i++){
+                fac *= i;
+            }
+            return fac;
         }
 };
+
+
+__global__ void setCofaces(Set s, int * nbrs, int cur_dim, int index);
+
+//Initial 0 Dim
+__global__ locateCofaces(Set s, int dim){
+    int index = threadIdx.x;
+    //Get lower neighbor indecies
+    int * nbrs = s.getLowers(index);
+
+    //Set the 0 dimension
+    dim3 zero_index(0,index,0);
+    s.d_setCoface(zero_index,index)
+
+    for(int d = 1; d < dim; d++){
+        setCofaces<<<1,1>>>(s,nbrs,d,index);
+        
+    }
+}
+
+__global__ void setCofaces(Set s, int * nbrs, int cur_dim, int index){
+    int index = s.ncr(index+1,cur_dim+1);
+    int total = s.ncr(index,cur_dim);
+
+    //Grab everything from previous layer
+    for(int i = 0; i < total;i++){
+
+    }
+
+
+
+}
